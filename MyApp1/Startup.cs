36dedd7881014d0ccbdd31b08c45199664e67b63
Tomasz Busłowski <tbuslowski@gmail.com>
2016-11-08 16:32:24 +0100

    init

---
+++ MyApp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyApp1.Startup))]
+namespace MyApp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
